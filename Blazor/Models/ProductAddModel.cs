5eb165a91b3b97d4c686dfa0532f16e94b864e23
rahmaniftaliev <rahmaniftaliev@gmail.com>
2026-08-14 12:56:23 +0400

    Adding additional validation features

--- Blazor/Models/ProductAddModel.cs
+++ Blazor/Models/ProductAddModel.cs
@@ -4,9 +4,12 @@ namespace Blazor.Models
 {
     public class ProductAddModel
     {
-        [Required]
+        [Required(ErrorMessage ="Product name is required")]
+        [StringLength(20,ErrorMessage = "Character limit exceeded(Max 20)")]
         public string? ProductName { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Product Description is required")]
+        [StringLength(40, ErrorMessage = "Character limit exceeded(Max 40)")]
         public string? Description { get; set; }
     }
 }
