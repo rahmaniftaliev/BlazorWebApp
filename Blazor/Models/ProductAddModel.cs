11c7878824b56dc01d83aadf15df5e7e597f4fc5
rahmaniftaliev <rahmaniftaliev@gmail.com>
2026-08-14 12:31:58 +0400

    validation application

---
+++ Blazor/Models/ProductAddModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.Models
+{
+    public class ProductAddModel
+    {
+        [Required]
+        public string? ProductName { get; set; }
+        [Required]
+        public string? Description { get; set; }
+    }
+}
