ac8dc56fecf6b101c910668a609bae1a2ca43972
rahmaniftaliev <rahmaniftaliev@gmail.com>
2026-08-15 18:21:21 +0400

    dependency injection introduction

---
+++ Blazor/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor.Models
+{
+    public class TodoItem
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ Blazor/Services/JsonPlaceHolderApi.cs
@@ -0,0 +1,23 @@
+﻿using Blazor.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+
+namespace Blazor.Services
+{
+    public class JsonPlaceHolderApi
+    {
+        private HttpClient _httpClient;
+
+        public JsonPlaceHolderApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<TodoItem[]> GetTodos()
+        {
+            return await _httpClient.GetFromJsonAsync<TodoItem[]>(
+                "https://jsonplaceholder.typicode.com/todos"
+            ) ?? [];
+        }
+    }
+}
