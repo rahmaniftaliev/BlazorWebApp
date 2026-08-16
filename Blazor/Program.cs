024b9227edb01124a4f881f992c47e6c1e8d2d9e
rahmaniftaliev <rahmaniftaliev@gmail.com>
2026-08-16 10:04:50 +0400

    Working with parameterized routes

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -8,6 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<IApiService,JsonPlaceHolderApi>();
+builder.Services.AddScoped<IApiService,JsonPlaceHolderApi>();
 
 await builder.Build().RunAsync();
